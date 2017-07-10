1848db690f90c7af3a7d96c3360534a446d724a7
Ignacio Avas <iavas@sophilabs.com>
2017-07-10 15:00:52 -0300

    Fix urls for tests

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8
-from __future__ import unicode_literals, absolute_import
-
-from django.conf.urls import url, include
-
-from django_api_query_count.urls import urlpatterns as django_api_query_count_urls
+from __future__ import absolute_import, unicode_literals
 
 urlpatterns = [
-    url(r'^', include(django_api_query_count_urls, namespace='django_api_query_count')),
 ]
